aa12b26c630a618944308357dcac1d288c08f335
Simon Täcklind <82391457+stacklind@users.noreply.github.com>
2024-02-20 14:38:10 +0100

    The displaying of an error is now done in the GUI class. (#13)

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GUI : MonoBehaviour
 {
     [SerializeField] private TargetingGUI targetingGUI;
+    [SerializeField] private TMP_Text errorMessageTextComponent;
 
     public void DisplayTargetingGUI(TargetAquisition targetAquisition)
     {
@@ -18,5 +19,26 @@ public class GUI : MonoBehaviour
     {
         GUIHandler.CancelTargetAquisition();
         targetingGUI.gameObject.SetActive(false);
+    }
+    
+    public void DisplayError(string errorText, float errorDisplayTime)
+    {
+        StartCoroutine(DisplayErrorCoroutine(errorText, errorDisplayTime));
+    }
+
+    private IEnumerator DisplayErrorCoroutine(string errorText, float errorDisplayTime)
+    {
+        errorMessageTextComponent.text = errorText;
+        errorMessageTextComponent.gameObject.SetActive(true);
+
+        float i = 0f;
+
+        while (i < errorDisplayTime)
+        {
+            i += Time.deltaTime;
+            yield return null;
+        }
+
+        errorMessageTextComponent.gameObject.SetActive(false);
     }
 }
